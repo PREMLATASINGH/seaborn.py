d02bed43951af4451c6522783a892d9828f13c2d
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-06 15:05:11 -0500

    first commit

---
+++ seaborn1.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+# Load the example tips dataset
+tips = sns.load_dataset("tips")
+# Create a scatter plot of total bill vs. tip
+sns.scatterplot(x="total_bill", y="tip", data=tips)
+# Show the plotplt.show()
+plt.show()
