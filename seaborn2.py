27764bc62d571c7476e346cd830722c34e692c64
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-06 15:32:17 -0500

    added

--- seaborn2.py
+++ seaborn2.py
@@ -3,3 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 df=pd.read_csv("data.csv")
 print(df.head())
+plt.figure(figsize=(10,6))
+sns.barplot(x="Category", y="Value", data=df)
+plt.title("Bar Plot of Category vs Value")
+plt.xlabel("Category")
+plt.ylabel("Value")
+plt.show()
