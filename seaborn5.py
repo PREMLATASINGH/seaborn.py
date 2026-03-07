64d7dd30053cd57b7f6b31e74fd790248ef7450a
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-06 22:46:09 -0500

    added

---
+++ seaborn5.py
@@ -0,0 +1,9 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+df=pd.DataFrame({
+    'month': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+    'sales': [100, 150, 200, 250, 300, 350],
+    'profit': [20, 30, 50, 70, 90, 110]
+})
+print(df.head())
