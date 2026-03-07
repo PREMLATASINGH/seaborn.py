13ccaa08d10631468914560a5ea70eb1747ef5b1
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-06 22:35:54 -0500

    added

---
+++ seaborn4.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+import matplotlib.pyplot as plt 
+import pandas as pd
+df=pd.DataFrame({
+    "Region": ["North", "South", "East", "West", "Central"],
+    "Value": [10, 15, 7, 12, 20]
+})
+print(df.head())
