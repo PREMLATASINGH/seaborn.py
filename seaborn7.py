cf811562b81dbeae5bd81e1cac51ceef09e8a813
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-08 00:13:21 -0500

    added

---
+++ seaborn7.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+df = pd.DataFrame({
+    "subject": ["Math", "Science", "English", "History", "Art"],
+    "score": [85, 90, 78, 92, 88]
+})
+print(df.head())
