7291ef457440cce3b11e9464159819431a76748a
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-11 14:16:24 -0400

    added describe data

--- seaborn8.py
+++ seaborn8.py
@@ -4,3 +4,4 @@ import pandas as pd
 df = pd.DataFrame({"sales": [200, 220, 250, 270, 300], "day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
                     "profit": [50, 60, 70, 80, 90], "expenses": [150, 160, 180, 190, 210]})
 print(df.head())
+print(df.describe())
