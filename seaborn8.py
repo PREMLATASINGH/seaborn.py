d4ddb3b0145ba563b299259e75ad6dbcb3cfccb1
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-11 14:15:37 -0400

    added new file

---
+++ seaborn8.py
@@ -0,0 +1,6 @@
+import seaborn as sns
+import matplotlib.pyplot as plt 
+import pandas as pd
+df = pd.DataFrame({"sales": [200, 220, 250, 270, 300], "day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+                    "profit": [50, 60, 70, 80, 90], "expenses": [150, 160, 180, 190, 210]})
+print(df.head())
